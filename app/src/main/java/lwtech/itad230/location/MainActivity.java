2e00799fdf41a200ffaedf1615097bfd89b4c58a
JacobLeMasters <SorrowsCB@gmail.com>
2016-02-21 18:47:45 -0800

    First Commit

--- app/src/main/java/lwtech/itad230/location/MainActivity.java
+++ app/src/main/java/lwtech/itad230/location/MainActivity.java
@@ -1,113 +1,52 @@
 package lwtech.itad230.location;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.location.Location;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
-import java.text.DateFormat;
-import java.util.Date;
 
-public class MainActivity extends AppCompatActivity
-        implements GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener,
-        LocationListener {
-
-    private GoogleApiClient mGoogleApiClient;
-    private Location mLastLocation;
-    private LocationRequest mLocationRequest;
-
-    private TextView mLatitude;
-    private TextView mLongitude;
-    private TextView mLastUpdate;
+/**
+ * Created by Jacob on 2/21/2016.
+ */
+public class MainActivity extends Activity{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        mLatitude = (TextView) findViewById( R.id.latitude );
-        mLongitude = (TextView) findViewById( R.id.longitude );
-        mLastUpdate = (TextView) findViewById( R.id.updatetime );
+    public void startMainService(View v){
+        Intent intent = new Intent(this, MainService.class);
+        startService(intent);
+    }
 
-        if( mGoogleApiClient == null ) {
-            mGoogleApiClient = new GoogleApiClient.Builder(this)
-                    .addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
-                    .addApi(LocationServices.API)
-                    .build();
+    public void stopMainService(View v){
+        Intent intent = new Intent(this, MainService.class);
+        stopService(intent);
+    }
+
+    public void openTextFile(View v) throws FileNotFoundException {
+        int i;
+        TextView textView = (TextView) findViewById(R.id.fileContents);
+        String message = textView.getText().toString();
+        File file = new File(getFilesDir(), "filename.txt");
+    try {
+        FileInputStream fileInputStream = new FileInputStream(file);
+        while ((i = fileInputStream.read()) != -1) {
+            message += (char) i;
         }
+        fileInputStream.close();
+    }catch (Exception e){
+        e.printStackTrace();
     }
 
-    @Override
-    public void onConnected(Bundle bundle) {
-        mLastLocation = LocationServices.FusedLocationApi.getLastLocation( mGoogleApiClient);
-
-        if( mLastLocation == null ) {
-            return;
-        }
-
-        mLatitude.setText(String.valueOf(mLastLocation.getLatitude()));
-        mLongitude.setText( String.valueOf(mLastLocation.getLongitude()));
-        String dat = DateFormat.getTimeInstance().format(new Date()).toString();
-        mLastUpdate.setText( dat );
-
-        createLocationRequest();
-        if( mLocationRequest != null){
-            startLocationUpdates();
-        }
+        textView.setText(message);
     }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-
-    }
-
-    @Override
-    protected void onStart() {
-        mGoogleApiClient.connect();
-        super.onStart();
-    }
-
-    @Override
-    protected void onStop() {
-        mGoogleApiClient.disconnect();
-        super.onStop();
-    }
-
-    protected void createLocationRequest() {
-        mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(10000);
-        mLocationRequest.setFastestInterval(5000);
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-    }
-
-    protected void startLocationUpdates() {
-        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, (LocationListener) this);
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        mLastLocation = location;
-
-        mLatitude.setText(String.valueOf(mLastLocation.getLatitude()));
-        mLongitude.setText( String.valueOf(mLastLocation.getLongitude()));
-        String dat = DateFormat.getTimeInstance().format(new Date()).toString();
-        mLastUpdate.setText( dat );
-    }
-
 }
---
+++ app/src/main/java/lwtech/itad230/location/MainService.java
@@ -0,0 +1,137 @@
+package lwtech.itad230.location;
+
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.DateFormat;
+import java.util.Date;
+
+public class MainService extends IntentService
+        implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
+
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+    private LocationRequest mLocationRequest;
+
+    private String mLatitude;
+    private String mLongitude;
+    private String mLastUpdate;
+    private File file;
+    private FileOutputStream fileOutputStream;
+    private String output;
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        mGoogleApiClient.disconnect();
+        mGoogleApiClient.connect();
+    }
+
+    public MainService(){
+        super("MainService");
+    }
+
+    @Override
+    public void onCreate(){
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
+        super.onCreate();
+    }
+
+    @Override
+    public void onDestroy(){
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation( mGoogleApiClient);
+
+        if( mLastLocation == null ) {
+            return;
+        }
+
+
+        file = new File(getFilesDir(), "filename.txt");
+        mLatitude = (String.valueOf(mLastLocation.getLatitude()));
+        mLongitude = (String.valueOf(mLastLocation.getLongitude()));
+        mLastUpdate = DateFormat.getTimeInstance().format(new Date());
+
+        output = "Latitude: " + mLatitude + '\n'
+                + "Longitude: " + mLongitude + '\n'
+                + "Time Stamp: " + mLastUpdate + '\n';
+        try {
+            fileOutputStream = new FileOutputStream(file);
+            fileOutputStream.write(output.getBytes());
+            fileOutputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        createLocationRequest();
+        if( mLocationRequest != null){
+            startLocationUpdates();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    protected void startLocationUpdates() {
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mLastLocation = location;
+
+        file = new File(getFilesDir(), "filename.txt");
+        mLatitude = (String.valueOf(mLastLocation.getLatitude()));
+        mLongitude = ( String.valueOf(mLastLocation.getLongitude()));
+        mLastUpdate = DateFormat.getTimeInstance().format(new Date());
+
+        output = "Latitude: " + mLatitude + '\n'
+                + "Longitude: " + mLongitude + '\n'
+                + "Time Stamp: " + mLastUpdate + '\n';
+        try {
+            fileOutputStream = new FileOutputStream(file);
+            fileOutputStream.write(output.getBytes());
+            fileOutputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
